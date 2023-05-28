9ec6e7a4c63084ab7b5baa1750e0af7d79d6073b
vincdeprinz <vincentstucki9@gmail.com>
2023-05-28 17:01:15 +0200

    28.05.2023

--- js/edit.js
+++ js/edit.js
@@ -2,6 +2,53 @@ var storedItem = localStorage.getItem("storedItem");
 var y = false;
 
 
+
+window.addEventListener('DOMContentLoaded', function() {
+  var erstelleNote = document.getElementById("erstelleNote");
+  var loescheFach = document.getElementById("loescheFach");
+  erstelleNote.style.display = "none";
+  loescheFach.style.display = "none";
+
+
+
+  var gespeicherteButtons = localStorage.getItem('gespeicherteButtons');
+  var buttonsArray = gespeicherteButtons ? JSON.parse(gespeicherteButtons) : [];
+
+  buttonsArray.forEach(function(fach) {
+    var gespeicherteWerte = localStorage.getItem('faecher');
+    var fach = JSON.parse(gespeicherteWerte);
+
+      var neuerButton = document.createElement('button');
+      neuerButton.textContent = fach;
+      neuerButton.className = 'button-28';
+      neuerButton.id = fach;
+
+      var createFach = document.getElementById('createFach');
+      createFach.style.display = "none";
+      var x = document.getElementById("bit");
+      x.style.display = "block";
+      
+      document.getElementById('fach').value = "";
+
+      neuerButton.addEventListener('click', function(){
+        var x = document.getElementById("bit");
+        var erstelleFach = document.getElementById("erstelleFach");
+        var erstelleNote = document.getElementById("erstelleNote");
+        var loescheFach = document.getElementById("loescheFach");
+
+        document.getElementById('titel').textContent = fach;
+            x.style.display = "none";
+            y = true;
+        erstelleFach.style.display = "none";
+        erstelleNote.style.display = "block";
+        loescheFach.style.display = "block";
+      });
+
+      var container = document.getElementById('bit');
+      container.appendChild(neuerButton);
+  });
+});
+
 function save(){
     var Item = document.getElementById("input").value;
     localStorage.setItem("storedItem", Item);
@@ -23,6 +70,12 @@ function back(){
             x.style.display = "block";
         }
         document.getElementById('titel').textContent = 'KBW';
+        var erstelleNote = document.getElementById("erstelleNote");
+        var loescheFach = document.getElementById("loescheFach");
+        var erstelleFach = document.getElementById("erstelleFach");
+        erstelleNote.style.display = "none";
+        loescheFach.style.display = "none";
+        erstelleFach.style.display = "block";
         y = false;
     }
 
@@ -65,10 +118,10 @@ function change(){
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  var ausloeser = document.getElementById('ausloeser');
+  var erstelleFach = document.getElementById('erstelleFach');
 
 
-  ausloeser.addEventListener('click', function() {
+  erstelleFach.addEventListener('click', function() {
     var createFach = document.getElementById('createFach');
     createFach.style.display = "block";
     var x = document.getElementById("bit");
@@ -80,15 +133,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 document.addEventListener('DOMContentLoaded', function() {
-  var ausloeser = document.getElementById('fachSubmit');
-
+    var fachSubmit = document.getElementById("fachSubmit");
     fachSubmit.addEventListener('click', function() {
       var fach = document.getElementById("fach").value;
 
       var neuerButton = document.createElement('button');
       neuerButton.textContent = fach;
       neuerButton.className = 'button-28';
-      neuerButton.id = 'btn2';
+      neuerButton.id = fach;
 
       var createFach = document.getElementById('createFach');
       createFach.style.display = "none";
@@ -99,16 +151,53 @@ document.addEventListener('DOMContentLoaded', function() {
 
       neuerButton.addEventListener('click', function(){
         var x = document.getElementById("bit");
-        document.getElementById('titel').textContent = 'Deutsch';
-        if(x.style.display === "none"){
-            x.style.display = "block";
-        } else{
+        var erstelleFach = document.getElementById("erstelleFach");
+        var erstelleNote = document.getElementById("erstelleNote");
+        var loescheFach = document.getElementById("loescheFach");
+
+        document.getElementById('titel').textContent = fach;
             x.style.display = "none";
-        }
-        y = true;
+            y = true;
+        erstelleFach.style.display = "none";
+        erstelleNote.style.display = "block";
+        loescheFach.style.display = "block";
       });
 
       var container = document.getElementById('bit');
       container.appendChild(neuerButton);
+
+      var gespeicherteButtons = localStorage.getItem('gespeicherteButtons');
+      localStorage.setItem('faecher', JSON.stringify(fach));
+      var buttonsArray = gespeicherteButtons ? JSON.parse(gespeicherteButtons) : [];
+     buttonsArray.push(fach); // Hier die ID des Buttons speichern
+     localStorage.setItem('gespeicherteButtons', JSON.stringify(buttonsArray));
   });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var loescheFach = document.getElementById('loescheFach');
+
+
+loescheFach.addEventListener('click', function() {
+  var x = document.getElementById("bit");
+  var erstelleFach = document.getElementById("erstelleFach");
+  var erstelleNote = document.getElementById("erstelleNote");
+  var loescheFach = document.getElementById("loescheFach");
+
+  var faecher = document.getElementById('titel');
+  var buttonId = faecher.textContent;
+  var button = document.getElementById(buttonId);
+
+  
+    x.style.display = "block";
+    erstelleNote.style.display = "none";
+    loescheFach.style.display = "none";
+    erstelleFach.style.display = "block";
+  if(button){
+    button.parentNode.removeChild(button);
+  }
+
+  document.getElementById('titel').textContent = "KBW";
+});
 });
